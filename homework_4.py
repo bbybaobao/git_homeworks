42d5c22f98c2bf794f0dfe357bda5487cfaee450
bbybaobao <cozykamiii@gmail.com>
2023-08-16 23:40:18 +0300

    Done Task 1

---
+++ homework_4.py
@@ -0,0 +1,13 @@
+"""Task 1"""
+
+# numbers = input("Enter numbers: ")
+#
+# duplicate = False
+# for char in numbers:
+#     if char.isdigit() and numbers.count(char) >= 2:
+#         duplicate = True
+#         print(f"{numbers} Yes")
+#         break
+#
+# if not duplicate:
+#     print(f"{numbers} No")

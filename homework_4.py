f769dc3e15ba8a77c0fb4571cdc8f3b4b258aa2f
bbybaobao <cozykamiii@gmail.com>
2023-08-16 23:41:13 +0300

    Done Task 3

--- homework_4.py
+++ homework_4.py
@@ -21,3 +21,32 @@
 #         print(f"{i}*{i}={square}")
 # # str(square).endswith(str(i)) -- проверяет,
 # # заканчивается ли число той же цифрой
+
+"""Task 3"""
+
+# numbers = []
+# total_sum = even_count = odd_count = 0
+#
+# while True:
+#     num = int(input("Enter numbers (to complete enter 0): "))
+#     if num == 0:
+#         break
+#     numbers.append(num)
+#
+# for num in numbers:
+#     total_sum += num
+#     if num % 2 == 0:
+#         even_count += 1
+#     else:
+#         odd_count += 1
+#
+# if numbers:
+#     average = total_sum / len(numbers)
+#     print(f"Total sum: {total_sum}")
+#     print(f"Average: {average}")
+#     print(f"Max: {max(numbers)}")
+#     print(f"Min: {min(numbers)}")
+#     print(f"Even: {even_count}")
+#     print(f"Odd: {odd_count}")
+# else:
+#     print("You enter 0.")
